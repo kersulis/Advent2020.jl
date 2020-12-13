b9c3ec2a1b1dad5ebfd4c08277ae6248ef7edff9
Jonas Kersulis <kersulis@umich.edu>
2020-12-13 18:46:00 -0500

    Forgot to wrap day 10 test groups

--- test/day10.jl
+++ test/day10.jl
@@ -2,15 +2,17 @@ using Advent2020.Day10
 
 input_path = joinpath(inputs_path, "day10.txt")
 
-@testset "Part 1" begin
-    input = parse.(Int64, readlines(input_path))
-    d = [diff(sort([input; 0])); 3]
-    @test sum(d .== 1) * sum(d .== 3) == 1890
-end
+@testset "Day 10" begin
+    @testset "Part 1" begin
+        input = parse.(Int64, readlines(input_path))
+        d = [diff(sort([input; 0])); 3]
+        @test sum(d .== 1) * sum(d .== 3) == 1890
+    end
 
-@testset "Part 2" begin
-    input = parse.(Int64, readlines(input_path))
-    v = [sort([input; 0]); maximum(input) + 3]
-    d = diff(v)
-    @test combos.(diff([0; findall(d .== 3)]) .- 1) |> prod == 49607173328384
+    @testset "Part 2" begin
+        input = parse.(Int64, readlines(input_path))
+        v = [sort([input; 0]); maximum(input) + 3]
+        d = diff(v)
+        @test combos.(diff([0; findall(d .== 3)]) .- 1) |> prod == 49607173328384
+    end
 end
